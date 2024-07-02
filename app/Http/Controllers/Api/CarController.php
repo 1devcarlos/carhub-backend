8775a49cad61327baab14c22daba32bf6423f793
Carlos Gomes <cg.dev1@hotmail.com>
2024-07-01 23:30:22 -0300

    Add => Car creation, update, photoUpload and deletion

---
+++ app/Http/Controllers/Api/CarController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Car;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+
+class CarController extends Controller
+{
+
+  public function index()
+  {
+    $cars = Car::all();
+
+    return response()->json(compact('cars'));
+  }
+  public function store(Request $request)
+  {
+    $validator = Validator::make($request->all(), [
+      'model' => 'required|string|max:255',
+      'brand' => 'required|string|max:255',
+      'year' => 'required|integer|min:1900|max:' . date('Y'),
+      'color' => 'required|string|max:255',
+      'daily_price' => 'required|numeric',
+      'status' => 'required|string|max:50',
+      'company_id' => 'required|exists:companies,id'
+    ]);
+
+    if ($validator->fails()) {
+      return response()->json($validator->errors(), 422);
+    }
+
+    $car = Car::create($request->all());
+
+    return response()->json(compact('car'), 201);
+  }
+
+  public function update(Request $request, $id)
+  {
+    $car = Car::find($id);
+
+    if (!$car) {
+      return response()->json(['error' => 'Car not found'], 404);
+    }
+
+    $validator = Validator::make($request->all(), [
+      'model' => 'sometimes|string|max:255',
+      'brand' => 'sometimes|string|max:255',
+      'year' => 'sometimes|integer|min:1900|max:' . date('Y'),
+      'color' => 'sometimes|string|max:255',
+      'daily_price' => 'sometimes|numeric',
+      'status' => 'sometimes|string|max:50',
+      'company_id' => 'sometimes|exists:companies,id'
+    ]);
+
+    if ($validator->fails()) {
+      return response()->json($validator->errors(), 422);
+    }
+
+    $car->fill($request->all());
+    $car->save();
+
+    return response()->json(compact('car'));
+  }
+
+  public function uploadPhoto(Request $request, $id)
+  {
+    $car = Car::find($id);
+
+    if (!$car) {
+      return response()->json(['error' => 'Car not found'], 404);
+    }
+
+    $validator = Validator::make($request->all(), [
+      'photo' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048', // Max 2MB
+    ]);
+
+    if ($validator->fails()) {
+      return response()->json($validator->errors(), 422);
+    }
+
+    if ($request->hasFile('photo')) {
+      $file = $request->file('photo');
+      if (!$file->isValid()) {
+        return response()->json(['error' => 'Invalid file upload'], 400);
+      }
+
+      try {
+        $fileName = uniqid() . '.' . $file->getClientOriginalExtension();
+        $filePath = $file->storeAs('photos', $fileName, 'public');
+        $fullPath = storage_path('app/public/' . $filePath);
+
+        $binaryPhoto = file_get_contents($fullPath);
+
+        Storage::delete($filePath);
+
+        $car->photo = base64_encode($binaryPhoto);
+        $car->save();
+
+        $message = "Photo uploaded successfully!";
+        return response()->json(compact('message', 'car'));
+      } catch (\Exception $e) {
+        return response()->json(['error' => 'Failed to upload photo', 'details' => $e->getMessage()], 500);
+      }
+    }
+  }
+
+  public function destroy($id)
+  {
+    $car = Car::find($id);
+    if (!$car) {
+      return response()->json(['error' => 'Car not found'], 404);
+    }
+
+    $car->delete();
+
+    return response()->json(['message' => 'Car deleted successfully']);
+  }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\CarController;
 use App\Http\Controllers\Api\CompanyController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\AuthController;
@@ -19,8 +20,16 @@ Route::middleware(['auth:api'])->group(function () {
   Route::put('/profile/update', [UserController::class, 'update']);
   Route::post('/profile/image', [UserController::class, 'uploadPhoto']);
 
+  //? Empresa
   Route::get('/company', [CompanyController::class, 'index']);
   Route::post('/company/create', [CompanyController::class, 'store']);
   Route::put('/company/update/{id}', [CompanyController::class, 'update']);
   Route::delete('/company/delete/{id}', [CompanyController::class, 'destroy']);
+
+  //? Carros
+  Route::get('/cars', [CarController::class, 'index']);
+  Route::post('/cars/create', [CarController::class, 'store']);
+  Route::put('/cars/update/{id}', [CarController::class, 'update']);
+  Route::post('/cars/uploadPhoto/{id}', [CarController::class, 'uploadPhoto']);
+  Route::delete('/cars/delete/{id}', [CarController::class, 'destroy']);
 });
